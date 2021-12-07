534bd5b1728257261a9a2440bbd91db7d931396a
Pakmanlie <62767966+Pakmanlie@users.noreply.github.com>
2021-12-07 00:48:25 -0500

    Started creating base line for enemy
    Sorry for being inactive, will continue tomorrow.

---
+++ Game Development Club Project/Assets/BulletBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBase : MonoBehaviour
+{
+    //Mean to be a root object to be inherited
+    private float Angle;
+    private float Speed;
+    public virtual void StartProjectile(float Angle, float Speed)
+    {
+        this.Angle = Angle;
+        this.Speed = Speed;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game Development Club Project/Assets/EnemyBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    //imma be honest i suck at public and private varibles ill work on it more later
+    //this class is meant to be inherited
+    private int Health;
+    [SerializeField]
+    private GameObject BulletPrefab;
+    //Instantiates a single object upon death, that object that can be used to spawn particles or show a single animation
+    [SerializeField]
+    private GameObject DestroyParticleEffect;
+    public virtual void DamageEnemy(int Damage)
+    {
+        Health -= Damage;
+        //Death
+        if (Health <= 0)
+        {
+            Instantiate(DestroyParticleEffect, gameObject.transform.position, Quaternion.identity.normalized);
+            Destroy(gameObject);
+        }
+    }
+    // Shoots a BulletPrefab at several angles at the parameters of the Angles. Angles and Speeds need to be the same
+    public virtual void Shoot(Vector3 ShootPosition ,GameObject BulletPrefab, float[] Angles, float[] Speeds)
+    {
+        for(int x = 0; x < Angles.Length; x++)
+        {
+            GameObject spawnedObject = Instantiate(BulletPrefab, ShootPosition, Quaternion.identity.normalized);
+
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
